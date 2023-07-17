677a5f1f6f94fe2ad312a74b66df85349c7dc86d
Tae Seo Moon / 문태서 <ansxotj06@naver.com>
2023-07-17 10:52:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def draw_parabola(): # 운동1 - 포물선 운동
   motion_graph = graph(title='position-time', xtitle='t', ytitle='y')
   g_bally = gcurve()
 
+<<<<<<< Updated upstream
   motion_graph = graph(title='velocity-time', xtitle='t', ytitle='vy')
   g_ballvy = gcurve(color=color.green)
 
@@ -41,6 +42,25 @@ def draw_parabola(): # 운동1 - 포물선 운동
     g_bally.plot(pos=(t, ball.pos.y))
     g_ballvy.plot(pos=(t, ball.v.y))
     t = t + dt
+=======
+    # 포물선운동
+    ball = sphere(radius = 0.2) # 물체 반지름
+    ground = box(pos=vec(0, -4, 0), size=vec(15, -0.01, 5))
+    ball.pos = vec(-2, 0, 0)
+    ball.v = vec(1, 1, 0)  # y값 0 : 제자리에서 포물선 운동, 1 : 위로 올라갔다가 떨어지는 포물선운동r
+    ball.a = vec(0, -0.35, 0)
+    attach_trail(ball, type='points', pps=6, color=color.red)
+    t = 0
+    dt = 0.01
+    while ball.pos.y > ground.pos.y:
+        rate(1 / dt)
+        ball.v = ball.v + ball.a * dt
+        ball.pos = ball.pos + ball.v * dt
+        t = t + dt
+    # 아래는 포물선 운동 그래프 구현
+    motion_graph = graph(title='position-time', xtitle='t', ytitle='y')
+    g_bally = gcurve()
+>>>>>>> Stashed changes
 
   max_y = ball.pos.y
   max_t = t

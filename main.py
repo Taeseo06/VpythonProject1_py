2ce11a87adfdb26dd9eda290363a17dd82b20871
Tae Seo Moon / 문태서 <ansxotj06@naver.com>
2023-08-26 13:12:19 +0900

    Update 8/26 (1) - 구조수정
    현재속도를 조정하는 방식에서 중력가속도를 조정하여 시뮬레이션 하는 방식으로 수정함

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ btn_motion1.place(x=400, y=20)
 
 # 포물선을 그리는 함수를 만듭니다.
 def draw_parabola(setting): # 운동1 - 포물선 운동
+
+    root.destroy()
+
     canvas(width=1300, height=900)
 
     # 포물선운동
@@ -24,7 +27,9 @@ def draw_parabola(setting): # 운동1 - 포물선 운동
     ball.pos = vec(-2, 0, 0)
     ball.v = vec(2, 2, 0) # 물체의 속도를 나타내는 벡터 -> y값 0 : 제자리에서 포물선 운동, 1 : 위로 올라갔다가 떨어지는 포물선운동
     # ball.a = vec(0, -0.35, 0) # 물체의 가속도를 나타내는 벡터 -> y값 : 중력의 정도
-    ball.a = vec(0, -2.0, 0)  # 물체의 가속도를 나타내는 벡터 -> y값 : 중력의 정도
+
+    ball.a = vec(0, -9.8, 0)  # 물체의 가속도를 나타내는 벡터 -> y값 : 중력의 정도
+    # ball.a = vec(2, -0.35, 0) # 물체의 가속도를 나타내는 벡터 -> y값 : 중력의 정도
 
     if setting['tracker'] == 1: # setting[tracker] 값에 따라 attach_trail 의 색상을 조정해서 가시/비가시로 구현함
         tracker_color = color.red
@@ -53,7 +58,7 @@ def draw_parabola(setting): # 운동1 - 포물선 운동
         ball.pos = ball.pos + ball.v * dt
         g_bally.plot(pos=(t, ball.pos.y))
         g_ballvy.plot(pos=(t, ball.v.y))
-        t = t + d
+        t = t + dt
 
     max_y = ball.pos.y
     max_t = t
@@ -75,7 +80,8 @@ def draw_parabola(setting): # 운동1 - 포물선 운동
 def setting_window(text): # 시뮬레이션 환경설정 윈도우 - (생성)
 
     setting = {'tracker' : 1,
-               'radius' : 0.2}
+               'radius' : 0.2,
+               'gravity_speed' : 0.35}
 
     settingWindow = Toplevel(root)
     settingWindow.title(f'{text} 시뮬레이션 환경 설정창')
@@ -86,10 +92,10 @@ def setting_window(text): # 시뮬레이션 환경설정 윈도우 - (생성)
     lb_tracker = Label(settingWindow, text = "현재상태 : " + ("ON" if setting['tracker'] else "OFF")) # 트랙커 상태 표시라벨
     lb_radius = Label(settingWindow, text="운동할 원의 반지름을 설정해주세요") # 반지름 설정 라벨
     spin_radius = Spinbox(settingWindow, from_=0.1, to=1.0, increment=.1, textvariable=setting['radius'], width=10) # 반지름 설정 스핀박스
-    lb_speed = Label(settingWindow, text="원의 운동 속도를 설정해주세요") # 운동속도 설정 라벨
+    lb_gravity_speed = Label(settingWindow, text="중력 가속도를 설정해주세요") # 중력가속도 설정 라벨
 
     combobox_var = StringVar()
-    combo_speed = ttk.Combobox(settingWindow, state="readonly", textvariable=combobox_var, values=['-4', '-3', '-2', '-1', '기본속도', '+1', '+2', '+3', '+4'], width=10) # 운동속도 설정 콤보박스
+    combo_gravity_speed = ttk.Combobox(settingWindow, state="readonly", textvariable=combobox_var, values=['-9.8', '-5', '-2', '-1', '기본 : 0.35', '1', '2', '5', '9.8'], width=10) # 중력가속도 설정 콤보박스
 
 
 
@@ -100,8 +106,8 @@ def setting_window(text): # 시뮬레이션 환경설정 윈도우 - (생성)
     lb_radius.place(x=155, y=120)
     spin_radius.place(x=193, y=140)
 
-    lb_speed.place(x=170, y=190)
-    combo_speed.place(x=165, y=230)
+    lb_gravity_speed.place(x=170, y=190)
+    combo_gravity_speed.place(x=165, y=230)
 
 
     def tracker_switch():
@@ -125,30 +131,19 @@ def setting_window(text): # 시뮬레이션 환경설정 윈도우 - (생성)
             print("반지름-올바른 숫자를 입력해주세요.")
 
     def on_combobox_select(event):
-        selected_value = combobox_var.get()
-        print(f'현재속도 : {selected_value}')
+        nonlocal setting
+        setting['gravity_speed'] = combobox_var.get()
+        print(f'현재 중력가속도 : {setting['gravity_speed']}')
 
-        # if selected_value == '기본속도':
-        # result_label.config(text="Selected value: " + selected_value)
+        # if gravity_speed == '기본속도':
+        # result_label.config(text="Selected value: " + gravity_speed)
 
     btn_start.config(command=lambda: draw_parabola(setting))
     btn_tracker.config(command=tracker_switch)
     spin_radius.config(command=get_radius)
     # combo_speed.config(command=)
-    combo_speed.bind("<<ComboboxSelected>>", on_combobox_select)
+    combo_gravity_speed.bind("<<ComboboxSelected>>", on_combobox_select)
 
-
-# 모션1 버튼 - 포물선운동 콜백함수
-def motion1_callback():
-  btn_motion1.destroy()
-
-  setting_window("포물선운동")
-
-
-
-
-  # draw_parabola() # 포물선 운동
-
-btn_motion1.config(command=motion1_callback)
+btn_motion1.config(command=lambda: setting_window("포물선운동")) # 메뉴 포물선 운동 버튼 피드백 함수 설정
 
 root.mainloop()
